070324c61530af3eb72c376787b9844c5aeb940f
Daniel Toledo <dan992965@gmail.com>
2023-01-25 18:19:26 -0300

    Feature/LayoutPadrao

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,30 +3,45 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon'
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabsMenuComponent } from './tabs-menu/tabs-menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmpresasComponent } from './empresas/empresas.component';
+import { AdministradoresComponent } from './administradores/administradores.component';
+import { ImplantacaoComponent } from './implantacao/implantacao.component';
+import { RelatoriosComponent } from './relatorios/relatorios.component';
+import { ComunicacaoComponent } from './comunicacao/comunicacao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    TabsMenuComponent,
+    DashboardComponent,
+    EmpresasComponent,
+    AdministradoresComponent,
+    ImplantacaoComponent,
+    RelatoriosComponent,
+    ComunicacaoComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      { path: '', component: DashboardComponent, pathMatch: 'full' },
+      { path: 'empresas', component: EmpresasComponent },
+      { path: 'administradores', component: AdministradoresComponent },
+      { path: 'implantacao', component: ImplantacaoComponent },
+      { path: 'relatorios', component: RelatoriosComponent },
+      { path: 'comunicacao', component: ComunicacaoComponent },
+    ]),
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
