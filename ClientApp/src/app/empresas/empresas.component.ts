5f41408db61bdb2b4213b0541cbe7bf086c4c2a2
Daniel Toledo <dan992965@gmail.com>
2023-01-31 16:45:54 -0300

    Feature/TelaEmpresas

--- ClientApp/src/app/empresas/empresas.component.ts
+++ ClientApp/src/app/empresas/empresas.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table'
+import { MatSort, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-empresas',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empresas.component.css']
 })
 export class EmpresasComponent implements OnInit {
+  displayedColumns: string[] = ['razaoSocial', 'cnpj', 'cadastroDate', 'vencimento', 'status'];
+  dataSource = new MatTableDataSource<EmpresasProps>(ELEMENT_DATA);
 
-  constructor() { }
+  constructor(private _liveAnnouncer: LiveAnnouncer) { }
 
-  ngOnInit(): void {
+  @ViewChild(MatPaginator) paginator: any = MatPaginator;
+  @ViewChild(MatSort) sort: any = MatSort;
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
+
+/* Static data */
+export interface EmpresasProps {
+  razaoSocial: string;
+  cnpj: string;
+  cadastroDate: string;
+  cadastroTime: string;
+  vencimento: string;
+  status: boolean;
+}
+const ELEMENT_DATA: EmpresasProps[] = [
+  { razaoSocial: "Mendes & Camargo Ltda", cnpj: "07.010.529/0001-35", cadastroDate: "11/10/2022", cadastroTime: "10:40", vencimento: "23/11/2023", status: true },
+  { razaoSocial: "Mendes & Camargo Ltda", cnpj: "07.010.529/0001-35", cadastroDate: "11/10/2022", cadastroTime: "10:40", vencimento: "23/11/2023", status: true },
+  { razaoSocial: "Mendes & Camargo Ltda", cnpj: "07.010.529/0001-35", cadastroDate: "11/10/2022", cadastroTime: "10:40", vencimento: "23/11/2023", status: false },
+  { razaoSocial: "Mendes & Camargo Ltda", cnpj: "07.010.529/0001-35", cadastroDate: "11/10/2022", cadastroTime: "10:40", vencimento: "23/11/2023", status: false },
+  { razaoSocial: "Mendes & Camargo Ltda", cnpj: "07.010.529/0001-35", cadastroDate: "11/10/2022", cadastroTime: "10:40", vencimento: "23/11/2023", status: false },
+];
